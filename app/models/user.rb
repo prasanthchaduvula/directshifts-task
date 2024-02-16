9f3c80b3d7da7fb856306c3a9c08965481664904
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2024-02-16 19:17:04 +0530

    added authentication token to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,21 @@ class User < ApplicationRecord
     presence: true,
     format: { with: /\A[a-zA-Z\s]+\z/ },
     length: { maximum: 20 }
+
+  before_save :ensure_authentication_token_is_present
+
+  private
+
+    def ensure_authentication_token_is_present
+      if authentication_token.blank?
+        self.authentication_token = generate_authentication_token
+      end
+    end
+
+    def generate_authentication_token
+      loop do
+        token = Devise.friendly_token
+        break token unless User.where(authentication_token: token).first
+      end
+    end
 end
