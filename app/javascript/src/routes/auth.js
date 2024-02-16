58129b30c21a71082706b33b7cf09e4ec4e8aa2b
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2024-02-16 15:50:59 +0530

    added signin components

--- app/javascript/src/routes/auth.js
+++ app/javascript/src/routes/auth.js
@@ -1,9 +1,14 @@
 import SignUp from "components/Authentication/Signup";
+import SignIn from "components/Authentication/Signin";
 
 export const AUTH_ROUTES = [
   {
     path: "/signup",
     component: SignUp,
+  },
+  {
+    path: "/signin",
+    component: SignIn,
   },
  
 ]
